48af6e95d12b5372512d0ebae9e53eb264e7ab92
Kamil Kuzmicki <kkuzmicki1988@gmail.com>
2020-08-27 09:35:05 +0200

    add class Product

--- src/js/script.js
+++ src/js/script.js
@@ -58,6 +58,7 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
       console.log('new Product:', thisProduct);
     }  
     renderInMenu(){
@@ -74,6 +75,33 @@
 
       /* add element to menu */   
       menuContainer.appendChild(thisProduct.element);
+    }
+    initAccordion(){
+      const thisProduct = this;
+    
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = document.querySelector(select.menuProduct.clickable);
+
+      /* START: click event listener to trigger */
+
+         /* prevent default action for event */
+
+        /* toggle active class on element of thisProduct */
+
+        /* find all active products */
+
+        /* START LOOP: for each active product */
+
+          /* START: if the active product isn't the element of thisProduct */
+
+            /* remove class active for the active product */
+
+          /* END: if the active product isn't the element of thisProduct */
+
+        /* END LOOP: for each active product */
+
+      /* END: click event listener to trigger */
+    
     }
   }
 
